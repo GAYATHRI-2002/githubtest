8468d8c3623b69ec757f137c0035476a3587d6fc
Gayathri <83536175+GAYATHRI-2002@users.noreply.github.com>
2023-08-16 21:57:50 +0530

    Create variable.c

---
+++ c program/variable.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a; //declaration
+    a = 123; // initialisation
+    printf("this is  %d a number \n", a);
+    float b = 4.5;
+    printf("my gpa is %f \n",b );
+     char grade = 'A';
+     printf("my grade is %c", grade);
+
+}
