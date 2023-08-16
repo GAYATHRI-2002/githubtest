49fc50f30dfae04bab555715efe11aeaafbd0ec7
Gayathri <83536175+GAYATHRI-2002@users.noreply.github.com>
2023-08-16 22:16:01 +0530

    Create datatypes.c

---
+++ c program/datatypes.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main(){
+
+    char a = 'C'; // single character
+    char d[] = "bro" ; // array of character
+
+    bool e = true;
+    printf("%d\n", e); // the answer will be 1 for true
+
+    bool ed = false;
+    printf("%d\n", ed); // the answer will be 0 for false
+
+    char f = 100;
+    printf("%d\n",f);
+    printf("%c",f);
+
+
+
+}
