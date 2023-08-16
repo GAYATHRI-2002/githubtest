06bc9a5494a3c4378d981abafe56053330a77756
Gayathri <83536175+GAYATHRI-2002@users.noreply.github.com>
2023-08-16 21:48:08 +0530

    Create hello.c

---
+++ c program/hello.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+
+
+    //this is a comment
+    /*this is a commet 
+    this is a multi comment
+    escape sequences : \n new line*/
+    printf("i\n like\n pizza\n");
+    printf("i like pizza it is really good\n");
+    printf("1\t2\t3\t4\t\n");
+    printf("\"i like dosa\" trump probably");
+    return 0 ;
+}
