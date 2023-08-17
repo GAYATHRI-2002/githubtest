90f76f2af1b64e45530e6f9511ef776ae7385e9d
Gayathri <83536175+GAYATHRI-2002@users.noreply.github.com>
2023-08-17 18:16:25 +0530

    Create arith.c

---
+++ c program/arith.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+   int x = 23;
+   int y = 34;
+   int z = x + y;
+   printf("%d\n", z);
+
+   int t = x - y;
+   printf("%d\n", t);
+
+    int p = x * y;
+   printf("%d\n", p);
+
+    float d = 2;
+    float e = 5;
+    float l = d / e;
+   printf("%.2f\n", l);
+
+   int k = 7;
+    int m = 2;
+    int n= k / m;
+   printf("%d", n);
+}
