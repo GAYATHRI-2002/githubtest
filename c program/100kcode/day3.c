50e47d6054bb6547e58b39ac0e3451ad364eda75
Gayathri <83536175+GAYATHRI-2002@users.noreply.github.com>
2024-02-18 12:58:23 +0530

    Update day3.c

--- c program/100kcode/day3.c
+++ c program/100kcode/day3.c
@@ -33,3 +33,29 @@ int main(){
         }
     }
 }
+
+// print prime numbers
+#include<stdio.h>                                                      
+int main(){
+    int num,i;
+    int flag = 0;
+    printf("enter a number ");
+    scanf("%d", &num);
+    
+   for(i=2;i<num/2;i++){
+    if(num%i==0){
+         flag=1;
+        break;
+    }
+   }
+   if(flag==1){
+    printf("%d not a prime number", num);
+   }
+   else{
+    printf("%d is a prime number", num);
+   }
+}
+
+
+    
+   
