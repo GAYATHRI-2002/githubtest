45fecae0d0a3a8c5a7634350242cdad6b3b86059
Gayathri <83536175+GAYATHRI-2002@users.noreply.github.com>
2024-02-21 06:43:52 +0530

    Create day6.c

---
+++ c program/100kcode/day6.c
@@ -0,0 +1,35 @@
+// selection sort
+#include<stdio.h>
+int main(){
+    int i, j, a[20], n, temp;
+
+    printf("enter the array size:");
+    scanf("%d", &n);
+
+    printf("enter the array : ");
+    for ( i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    printf("the array :");
+    for ( i = 0; i < n; i++){
+        printf(" %d", a[i]);
+    }
+
+    for ( i = 0; i < n-1; i++){
+        for ( j = i+1; j < n; j++) {
+            if(a[i]>a[j]){
+                 temp = a[i];
+                 a[i] = a[j];
+                 a[j] = temp;
+                }
+            }
+        
+    }
+
+    printf("\nthe sorted array : ");
+    for ( i = 0; i < n; i++){
+      printf(" %d", a[i]);  
+    }
+    
+}
